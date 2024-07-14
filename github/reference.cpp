73ce0c846b0594fc797309e6005f2ac0e879647d
JZG12823 <123844200+JZG12823@users.noreply.github.com>
2024-07-14 16:14:05 -0700

    upload 1 of study material [7.14.24 ; 4:12]

---
+++ github/reference.cpp
@@ -0,0 +1,11 @@
+//reference
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 0;
+    int &p = a;
+    cin >> a;
+    cout <<  a << "\n";
+    cout << p << "\n";
+    cout << &p << "\n";
+}
